5922259f80ab1868fb041e11a999c330320e3550
Shabrina <myrina.ptik03@gmail.com>
2019-03-03 16:27:16 +0700

    initial commit

---
+++ src/algen/AllOnesGA.java
@@ -0,0 +1,30 @@
+package algen;
+
+public class AllOnesGA {
+    public static void main(String[] args) {
+        // Create GA object
+        GeneticAlgorithm ga = new GeneticAlgorithm(50, 0.01, 0.95, 0);
+        // Initialize population
+        Population population = ga.initPopulation(5);
+
+        // The following is the new code you should be adding:
+        ga.evalPopulation(population);
+        int generation = 1;
+        while (ga.isTerminationConditionMet(population) == false) {
+            // Print fittest individual from population
+            System.out.println("Best solution: " + population.getFittest(0).toString());
+            // Apply crossover
+            population = ga.crossoverPopulation(population);
+            // Apply mutation
+            population = ga.mutatePopulation(population);
+            // Evaluate population
+            ga.evalPopulation(population);
+            // Increment the current generation
+            generation++;
+        }
+        System.out.println("Found solution in " + generation + " generations");
+        System.out.println("Best solution: " + population.getFittest(0).toString());
+        // We’ll add a lot more here...
+    }
+
+}
